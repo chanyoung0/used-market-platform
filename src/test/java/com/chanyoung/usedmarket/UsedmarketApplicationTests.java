0efdb70501a06204e7f607cebf148b5dc1a9426c
chanyoung0 <jcy3974@gmail.com>
2026-09-11 15:47:00 +0900

    chore : 프로젝트 초기 설정

---
+++ src/test/java/com/chanyoung/usedmarket/UsedmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanyoung.usedmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
