f07c08bc07b90dddf41ffd0259b0c2f19f187203
chanyoung0 <jcy3974@gmail.com>
2026-09-19 12:37:57 +0900

    feat: MemberRepository 추가
    - JpaRepository<Member, Long> 상속으로 기본 CRUD 확보
    - existsByEmail, findByEmail 추가 (회원가입 중복 체크, 로그인 조회용)

--- src/main/java/com/chanyoung/usedmarket/domain/member/MemberRepository.java
+++ src/main/java/com/chanyoung/usedmarket/domain/member/MemberRepository.java
@@ -1,4 +1,16 @@
 package com.chanyoung.usedmarket.domain.member;
 
-public class MemberRepository {
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
+
+
 }
